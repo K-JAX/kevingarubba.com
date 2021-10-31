9d1114deaa9bbcfa26d7c957d8dfe649fd5b9784
Kevin Garubba <kevingarubba@gmail.com>
2021-10-31 17:53:33 -0400

    Removing comments. Adding greeting to console.log.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -7,10 +7,7 @@
 
     import { durVal } from "./Components/Functional/SiteLoadCookie.svelte";
 
-    // console.log("performing fetch");
-    // fetch("http://localhost:8080/wp-json")
-    //     .then(res => res.json())
-    //     .then(data => console.log(data));
+    console.log("Hello ;)");
 </script>
 
 <div class="svelte-root flex flex-row" transition:fade="{{ duration: durVal }}">
