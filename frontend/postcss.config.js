95c15ad6d3b1165ca524f2ad884f06888ed1eca2
Kevin Garubba <kevingarubba@gmail.com>
2021-11-21 23:15:10 -0500

    Reordering safelist to see if that does anything.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -3,8 +3,8 @@ const tailwindcss = require("tailwindcss");
 // only needed if you want to purge
 const purgecss = require("@fullhuman/postcss-purgecss")({
     content: ["./src/**/*.svelte", "./public/**/*.html"],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    safelist: ["bg-cyan"]
+    safelist: ["bg-cyan"],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 
 module.exports = {
