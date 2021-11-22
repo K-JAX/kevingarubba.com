199c53d4bdabaa551247d1d739caa3aea1667615
Kevin Garubba <kevingarubba@gmail.com>
2021-11-22 16:38:39 -0500

    Updating the purgecss 'safelist' to 'whitelist' since that is what it is called in the older version I am using.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -3,7 +3,7 @@ const tailwindcss = require("tailwindcss");
 // only needed if you want to purge
 const purgecss = require("@fullhuman/postcss-purgecss")({
     content: ["./src/**/*.svelte", "./public/**/*.html"],
-    safelist: ["bg-cyan"],
+    whitelist: ["bg-cyan"],
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 
